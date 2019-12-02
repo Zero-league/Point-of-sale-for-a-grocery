1c23487fb771fce25341ba71669ee07a43888d5e
SaumyaWickramarathne <saumyalakmali999@gmail.com>
2019-12-02 21:53:53 +0530

    Models modification
    Modified the sales and bill models

---
+++ PointOfSalesForAGrocery/POS.Models/Bill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace POS.Models
+{
+    public class Bill
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BillId { get; set; }
+
+        public DateTime DateTime { get; set; }
+        public int Discount { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+        public string SalesPerson { get; set; }
+    }
+}
