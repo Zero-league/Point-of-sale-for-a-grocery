047db83c9f0c3a7cad6676ff67c789e089b5a897
Chanuga <Chanugatharindu1995@gmail.com>
2019-12-03 15:11:01 +0530

    Create Model for Expenses(Expenses,ExpenseType)

---
+++ PointOfSalesForAGrocery/POS.Models/ExpenseType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace POS.Models
+{
+    public class ExpenseType
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string ExpenseName { get; set; }
+
+    }
+}
---
+++ PointOfSalesForAGrocery/POS.Models/Expenses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace POS.Models
+{
+    public class Expenses
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ExpId { get; set; }
+
+        [ForeignKey("Id")]
+        public int ExpeId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public double Payment { get; set; }
+
+    }
+}
